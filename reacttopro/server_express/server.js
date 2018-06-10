795dda745299b47c7b7fd7024718d55ad5006b65
ANANDA\karoon <karoon@ananda.co.th>
2018-06-10 14:38:16 +0700

    Buid method Post and config middle-ware on express

--- reacttopro/server_express/server.js
+++ reacttopro/server_express/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+/*
+    command 
+        "app.use" This command use to tell the express ,that has config middle ware
+*/
+app.use(bodyParser.urlencoded({ extended: true }))  //config for support form submit
+app.use(bodyParser.json({ type: '*/*' })); //config for support body JSON
 
 
 app.get('/', (req, res) => {
@@ -21,6 +29,11 @@ app.get('/order/:orderId', (req, res) => {
     // http://localhost:8001/order/2
     res.send('Order Page Id = '+req.params.orderId) 
 });
+
+app.post('/order', (req, res) => {
+    console.log(req.body)    
+    res.json({ status: 'ok' }) 
+}); 
 
 app.listen(8001,()=>{
     console.log('Ready on http://localhost:8001')
