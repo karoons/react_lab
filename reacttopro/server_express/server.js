5d24e1b026f85119333baec6d92ee2151a1a5ba3
ANANDA\karoon <karoon@ananda.co.th>
2018-06-10 15:31:17 +0700

    Test Application Middleware concept

--- reacttopro/server_express/server.js
+++ reacttopro/server_express/server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))  //config for support form submit
 app.use(bodyParser.json({ type: '*/*' })); //config for support body JSON
 
+app.use((req, res, next) => {    
+    console.log('middle ware')    
+    next() 
+}) ;
+
 
 app.get('/', (req, res) => {
     // http://localhost:8001
@@ -17,11 +22,13 @@ app.get('/', (req, res) => {
 
 app.get('/member', (req, res) => {
     // http://localhost:8001/member
+    console.log('Member Page')
     res.send('Member Page') 
 });
 
 app.get('/order', (req, res) => {
     // http://localhost:8001/order
+    console.log('Order page')
     res.send('Order Page') 
 });
 
@@ -34,6 +41,24 @@ app.post('/order', (req, res) => {
     console.log(req.body)    
     res.json({ status: 'ok' }) 
 }); 
+
+app.get('/user/:id', function (req, res, next) {
+    // if the user ID is 0, skip to the next route
+    if (req.params.id === '0') next('route')
+    // otherwise pass the control to the next middleware function in this stack
+    else next()
+  }, function (req, res, next) {
+    // render a regular page
+    res.send('regular') 
+    // res.render('regular')
+  });
+  
+  // handler for the /user/:id path, which renders a special page
+  app.get('/user/:id', function (req, res, next) {
+    // res.render('special')
+    // res.send('special') 
+    res.json({success : 'special'}) 
+  });
 
 app.listen(8001,()=>{
     console.log('Ready on http://localhost:8001')
